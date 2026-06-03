77b55e56c28e7806644bb7b814b03973ca2c4c2f
Austin Shirley <austin@deadband.dev>
2026-06-03 14:23:00 -0600

    feat: Update to Buildroot 2025.02.14 and nixpkgs
    Includes:
    * kernel 6.12.90
    * openssl 3.5.6
    * latest 25.11 release nixpkgs
    
    Updated SECURITY.md for improved clarity and response timeline

--- inputs.nix
+++ inputs.nix
@@ -7,13 +7,13 @@
 # so parent flakes do NOT need to redeclare these inputs. Bump a pin = edit
 # this file, commit, bump submodule pointer in parent
 {
-  nixpkgs.url = "github:NixOS/nixpkgs/7e495b747b51f95ae15e74377c5ce1fe69c1765f";
+  nixpkgs.url = "github:NixOS/nixpkgs/535f3e6942cb1cead3929c604320d3db54b542b9";
   buildroot-nix.url = "github:velentr/buildroot.nix/a9090cd64ce2b595a68b2acf2f13463b75673d80";
   # Non-flake inputs: lib.nix uses builtins.fetchGit, which requires
   # a full-SHA rev to be allowed in pure eval mode
   buildroot = {
     url = "https://gitlab.com/buildroot.org/buildroot.git";
-    rev = "137a566bf5353831d97c068a4c22e6df5b4d07a0";
+    rev = "898251ee2b83a9cd5ae0ae5db57828035a5a6f85";
     flake = false;
   };
 }
